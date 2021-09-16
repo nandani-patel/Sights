9594fcac7a48bac28a47b21ebaba623f631d7fff
nandani-patel <nandanip271@gmail.com>
2021-09-16 18:37:21 +0530

    routing

--- react-frontend/src/app.js
+++ react-frontend/src/app.js
@@ -1,16 +1,17 @@
 import React from 'react';
-
+import {Route} from 'react-router-dom';
 import Login from "./login";
-// import Map from "./Map";
+import Map from "./Map";
 import Post from "./posts";
 import BucketList from './BucketList';
 
 function App(){
     return (
         <div classname="App">
-            <Login/>
-            <BucketList/>
-            <Post/>
+           <Route exact path="/" to component={Login}/>
+           <Route exact path="/posts" to component={Post}/>
+           <route exact path="/Map" to component={Map}/>
+           <Route exact path="/BucketList" to component={BucketList}/>
         </div>
     );  
 }
